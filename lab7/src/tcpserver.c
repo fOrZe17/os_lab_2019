f7e5a10f10b0e50a11b2bb8238757316d09d53f6
fOrZe17 <vasilievdaniil789@gmail.com>
2024-12-13 22:08:32 +0000

    Добавлены файлы

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
+int main(int argc, char *argv[]) {
   const size_t kSize = sizeof(struct sockaddr_in);
-
   int lfd, cfd;
   int nread;
-  char buf[BUFSIZE];
+  int bufsize = atoi(argv[2]);
+  char *buf = malloc(bufsize);
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
+
+  if (argc < 3) {
+    printf("Too few arguments \n");
+    exit(1);
+  }
 
   if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("socket");
@@ -28,7 +30,7 @@ int main() {
   memset(&servaddr, 0, kSize);
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
+  servaddr.sin_port = htons(atoi(argv[1]));
 
   if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
     perror("bind");
@@ -49,8 +51,8 @@ int main() {
     }
     printf("connection established\n");
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
+    while ((nread = read(cfd, buf, bufsize)) > 0) {
+      write(1, buf, nread);
     }
 
     if (nread == -1) {
@@ -59,4 +61,6 @@ int main() {
     }
     close(cfd);
   }
+
+  free(buf);
 }
